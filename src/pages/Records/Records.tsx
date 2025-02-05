adc1e2a9d31e9fae5179a20641b0fad14ac46616
Sujith Barve <sujith.baruve@dhiway.com>
2025-02-05 13:32:46 +0530

    fix: Remove revoke and edit
    Signed-off-by: Sujith Barve <sujith.baruve@dhiway.com>

--- src/pages/Records/Records.tsx
+++ src/pages/Records/Records.tsx
@@ -1,77 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
-import { useToast } from "src/hooks/use-toast";
-import axios, { isAxiosError } from "axios";
-import { useParams } from "@tanstack/react-router";
 import RecordsTable from "@/components/RecordsTable";
 // import useAuthStore from "src/store/authStore";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Records = () => {
   // const auth = useAuthStore((state: any) => state.auth);
-  const { toast } = useToast();
-  const { nameSpaceId, directory } = useParams({
-    from: "/records/$creatorId/$nameSpaceId/$directory",
-  });
   const [refreshTable, setRefreshTable] = useState(0);
-  const [revokeEntries, setRevokeEntries] = useState(false);
-  const [schemaProperties, setSchemaProperties] = useState<any[]>([]);
   const [registryInfo, setregistryInfo] = useState(false);
   const [showRecordDetails, setShowRecordDetails] = useState(false);
   const [recordDetailsSingle, setRecordDetailsSingle] = useState();
   // const [recordEditEntries, setRecordEditEntries] = useState<any[]>([]);
-
-
-  const [createModalShow, setCreateModalShow] = useState(false);
-
-
-  const openRecordCreateModal = (isOpen: boolean): void => {
-    setCreateModalShow(isOpen);
-  }
-
-  // Fetch schema details dynamically
-  const fetchschemaDetails = async () => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_API_ENDPOINT}/dedi/lookup/${nameSpaceId}/${directory}`,
-        { withCredentials: true }
-      );
-      const schema = response?.data?.schema;
-      setregistryInfo(response?.data);
-      setSchemaProperties(schema);
-    } catch (error) {
-      if (isAxiosError(error)) {
-        const resData = error.response;
-        const status = resData?.status;
-        const key = Object.keys(resData?.data || {})[0];
-        toast({
-          variant: "destructive",
-          title: `${key} - ${status}` || `error - ${status}`,
-          description: resData?.data[key],
-        });
-      }
-    }
-  };
-
-  useEffect(() => {
-    fetchschemaDetails();
-  }, []);
-
-  const setRecordUpdate = (data: RecordUpdateData) => {
-    setRecordDataNotEdited(data);
-    let objectConstructed = {
-      recordName: data.details.recordName,
-      description: data.details.description,
-      ...data?.details?.details,
-    };
-    let wantedData = Object.keys(objectConstructed).map((key) => ({
-      [key]: objectConstructed[key],
-    }));
-    setEditingData(true);
-    setEnteredRecordData(wantedData);
-  };
-
   const showSingleRecords = (data) => {
     setShowRecordDetails(true);
     setRecordDetailsSingle(data);
@@ -85,9 +25,6 @@ const Records = () => {
         <div className=" mt-2">
           <RecordsTable
             refreshTable={refreshTable}
-            openRecordCreateModal={(isOpen: boolean) =>
-              openRecordCreateModal(isOpen)
-            }
             singleRecordDetails={(data) => showSingleRecords(data)}
           />
         </div>
@@ -139,16 +76,6 @@ const Records = () => {
                       </tr>
                     );
                   })}
-                  {/* {recordDetailsSingle.details.map((field: any) => (
-                    <tr key={field.key} className="even:bg-gray-50">
-                      <td className="border border-gray-300 px-4 py-2 text-sm">
-                        {formattedLabel(field.key)}
-                      </td>
-                      <td className="border border-gray-300 px-4 py-2 text-sm">
-                        {field.type}
-                      </td>
-                    </tr>
-                  ))} */}
                 </tbody>
               </table>
             )}
