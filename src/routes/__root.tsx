8cc5fe48b1381bda1ece79a789b942f7340bf550
Sujith Barve <sujith.baruve@dhiway.com>
2025-02-06 21:32:14 +0530

    bundle added
    Signed-off-by: Sujith Barve <sujith.baruve@dhiway.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,9 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import Header from "@/components/header/Header";
 import { Toaster } from "@/components/ui/toaster";
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <Header />
             <main>
                 <Outlet />
                 <Toaster />
