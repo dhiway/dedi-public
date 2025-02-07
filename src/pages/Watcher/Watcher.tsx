b1556ecdf88c145a58550d7a1054eef06aa9c689
RCCodeBase <rahultdpz143@gmail.com>
2025-02-07 15:49:28 +0530

    fix: text color change

--- src/pages/Watcher/Watcher.tsx
+++ src/pages/Watcher/Watcher.tsx
@@ -135,7 +135,7 @@ const Watcher = () => {
                             />
                             <Button
                                 id="first-create"
-                                className="cursor-pointer bg-transparent mt-3 hover:bg-blue-500 text-blue-700 font-semibold hover:text-gray-800 py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                                className="cursor-pointer bg-transparent mt-3 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
                                 onClick={() => {
                                     toast({
                                         variant: "default",
